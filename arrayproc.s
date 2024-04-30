c575eb5d635b6e28633c3d5741ce9765da35b7c4
James Clarke <james.clarke@tcu.edu>
2024-04-30 18:44:23 -0500

    copy, swap functions

--- arrayproc.s
+++ arrayproc.s
@@ -12,14 +12,25 @@ main:
   mov   x1,   #10
   bl    init_array
 
-  // Call print_array
-  adr   x0,   array
-  mov   x1,   #10
-  bl    print_array
+//   Call print_array
+//   adr   x0,   array
+//   mov   x1,   #10
+//   bl    print_array
+
+  // Call copy_array
+  adr   x0,   array_copy
+  adr   x1,   array
+  mov   x2,   #10
+  bl    copy_array
+
+  // Call swap
+  adr   x0,   a
+  adr   x1,   b
+  bl    swap
 
   // Epilog
-  mov   x0,   0
-  ret
+  mov   x8,   #93
+  svc   0
 
 // Initialize array function
 // x19 = pointer to first element of array
@@ -59,7 +70,6 @@ init_loop:
 
 init_end:
   // Epilog
-  mov   x0,   x19
   ldr   x19,  [sp, 8]
   ldr   x20,  [sp, 16]
   ldr   x21,  [sp, 24]
@@ -126,9 +136,88 @@ print_end:
   ldp   x29,  x30,  [sp],   #16
   ret
 
+// Copy array function
+// x19 = pointer to destination array
+// x20 = pointer to source  array
+// x21 = number of elements in both arrays
+// x22 = counter
+.global  copy_array
+copy_array:
+  // Prolog
+  stp   x29,  x30,  [sp, #-16]!
+  sub   sp,   sp,   32
+
+  str   x19,  [sp, 8]
+  str   x20,  [sp, 16]
+  str   x21,  [sp, 24]
+  str   x22,  [sp, 32]
+
+  // Store data
+  mov 	x19,  x0
+  mov   x20,  x1
+  mov   x21,  x2
+
+  // Initialize counter to 0
+  mov   x22,  #0
+
+copy_loop:
+  // Compare counter, n
+  cmp   x22,  x21
+  b     copy_end
+
+  // Load and store data
+  ldr   w2,  [x20, x22, lsl 2]
+  str   w2,  [x19, x22, lsl 2]
+
+  // Increment counter
+  add  x22,  x22,  #1
+  b    copy_loop
+
+copy_end:
+  // Epilog
+  ldr   x19,  [sp, 8]
+  ldr   x20,  [sp, 16]
+  ldr   x21,  [sp, 24]
+  ldr   x22,  [sp, 32]
+  add   sp,   sp,   32
+  ldp   x29,  x30,  [sp],   #16
+  ret
+
+// Swap integers function
+// x19 = pointer to a
+// x20 = pointer to b
+// x2,x3 = temp storage
+.global  swap
+swap:
+  // Prolog
+  stp   x29,  x30,  [sp, #-16]!
+  sub   sp,   sp,   16
+
+  str   x19,  [sp]
+  str   x20,  [sp, 8]
+
+  // Store data
+  mov   x19,  x0
+  mov   x20,  x1
+
+  // Load value of a into x2
+  ldr   x2,   [x19]
+  ldr   x3,   [x20]
+
+  str   x3,   [x19]
+  str   x2,   [x20]
+
+  // Epilog
+  ldr   x19,  [sp]
+  ldr   x20,  [sp, 8]
+  add   sp,   sp,   16
+  ldp   x29,  x30,  [sp],   #16
+  ret
+
 // Random number generator in range 0-255
 // x19 = temporarily holds file descriptor info
 // x20 = temporarily holds returned random integer
+.global  rand
   rand:
   // Prolog
   sub   sp,   sp,   32
@@ -169,7 +258,7 @@ print_end:
   add   sp,   sp,   32
   ret
 
-.section  .rodata
+.section  ".rodata"
 dev_urandom:
   .asciz  "/dev/urandom"
 format_string:
@@ -179,6 +268,14 @@ tab_string:
 newline_string:
   .string "\n"
 
-.section  .bss
+.section  ".bss"
 array:
   .skip   40
+array_copy:
+  .skip   40
+
+.section  .data
+a:
+  .word   10
+b:
+  .word   20
